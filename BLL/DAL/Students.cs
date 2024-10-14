a5bd6f8783ab4e301bb03409ce4355d9c67247ba
EkulerCuha <41061840+EkulerCuha@users.noreply.github.com>
2024-10-14 21:22:56 +0300

    Add files via upload

---
+++ BLL/DAL/Students.cs
@@ -0,0 +1,10 @@
+namespace BLL.DAL;
+
+public class Students
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string surname { get; set; }
+    public DateTime birthday { get; set; }
+    public double gpa { get; set; }
+}
